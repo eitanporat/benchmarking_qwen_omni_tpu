5a813955c2ab09b56e5353924e27b93a0958d79d
Eitan Porat <eitan.porat@weizmann.ac.il>
2025-10-21 14:14:35 +0000

    Add all VLLM environment flags from buildkite config

--- setup.sh
+++ setup.sh
@@ -15,9 +15,36 @@ export HF_HOME=/dev/shm
 export XLA_FLAGS="--xla_dump_to=/home/eporat/benchmarking_qwen_omni_tpu"
 export JAX_COMPILATION_CACHE_DIR=/home/eporat/benchmarking_qwen_omni_tpu
 export JAX_ENABLE_COMPILATION_CACHE=1
+export VLLM_XLA_CACHE_PATH="$JAX_COMPILATION_CACHE_DIR/.cache/jax_cache"
+export VLLM_USE_V1=1
+export VLLM_XLA_CHECK_RECOMPILATION=1
+
+# Optional environment variables (uncomment as needed)
+# export MODEL_IMPL_TYPE=flax_nnx
+# export QUANTIZATION=
+# export NEW_MODEL_DESIGN=
+# export USE_V6E8_QUEUE=
+# export JAX_RANDOM_WEIGHTS=
+# export SKIP_ACCURACY_TESTS=
+# export VLLM_MLA_DISABLE=
+
+# Benchmark configuration variables
+# export TEST_MODEL=
+# export MINIMUM_ACCURACY_THRESHOLD=
+# export MINIMUM_THROUGHPUT_THRESHOLD=
+# export TENSOR_PARALLEL_SIZE=
+# export INPUT_LEN=
+# export OUTPUT_LEN=
+# export PREFIX_LEN=
+# export MAX_MODEL_LEN=
+# export MAX_NUM_SEQS=
+# export MAX_NUM_BATCHED_TOKENS=
 
 # Add to bashrc for persistence
 echo 'export HF_HOME=/dev/shm' >> ~/.bashrc
 echo 'export XLA_FLAGS="--xla_dump_to=/home/eporat/benchmarking_qwen_omni_tpu"' >> ~/.bashrc
 echo 'export JAX_COMPILATION_CACHE_DIR=/home/eporat/benchmarking_qwen_omni_tpu' >> ~/.bashrc
 echo 'export JAX_ENABLE_COMPILATION_CACHE=1' >> ~/.bashrc
+echo 'export VLLM_XLA_CACHE_PATH="$JAX_COMPILATION_CACHE_DIR/.cache/jax_cache"' >> ~/.bashrc
+echo 'export VLLM_USE_V1=1' >> ~/.bashrc
+echo 'export VLLM_XLA_CHECK_RECOMPILATION=1' >> ~/.bashrc
